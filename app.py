80dd6274a45874ffd196ebf223ccca6a6981e7f0
Esben <116391208+Esben-8@users.noreply.github.com>
2023-05-13 19:48:00 +0200

    Upstash.py

--- app.py
+++ app.py
@@ -1,69 +1,86 @@
-from flask import Flask, request, jsonify, make_response
-from pytube import YouTube
+from flask import Flask, request, jsonify
+import redis
+import uuid
+import requests
+import json
+import time
 import openai
 import tempfile
+import pytube
 import os
-import traceback
-import logging
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
-# configure the logging module to write messages to a file
-logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+# Connect to Upstash Redis endpoint
+r = redis.Redis(host='eu1-boss-goblin-39389.upstash.io', port=39389, password="aad265ed603b471fa7346db6e5f27066")
 
-@app.route("/")
-def hello():
-    print("hej")
-    return "Hello"
+# Set up a route for video transcription request
+@app.route('/transcribe', methods=['POST'])
+def transcribe_video():
+    # Generate a unique job ID
+    job_id = str(uuid.uuid4())
+    
+    # Get video URL from the request
+    video_url = request.form['video_url']
+    max_length = request.form["max_length"]
+    api_key = request.form["api_key"]
+    
+    # Push job ID and video URL to Upstash QStash queue
+    r.lpush('qstash:transcription_jobs', json.dumps({'job_id': job_id, 'video_url': video_url, "max_length": max_length, "api_key": api_key}))
+    
+    # Respond to user with job ID and message
+    return jsonify({'job_id': job_id, 'message': 'Transcription job is being processed.'})
 
-@app.route("/transcribe", methods=["GET"])
-def transcribe():
-    try:
-        video_id = str(request.args.get("id"))
-        url = f"https://www.youtube.com/watch?v={video_id}"
-        # print(url)
-        max_length = int(request.args.get("max_length"))
-        apikey = str(request.args.get("api_key"))
-        print(apikey)
+# Set up a route for job status request
+@app.route('/status/<job_id>', methods=['GET'])
+def get_job_status(job_id):
+    # Get job status from the database
+    job_status = r.get('job:{}:status'.format(job_id))
+    
+    if job_status is None:
+        return jsonify({'message': 'Job not found.'}), 404
+    
+    # Respond to user with job status
+    return jsonify({'job_id': job_id, 'status': job_status.decode('utf-8')})
 
-        openai.api_key = apikey
-        temp_path = tempfile.gettempdir()
-        yt = YouTube(url)
-
-        print("Downloading...")
-        yt.streams.filter(only_audio=True, file_extension="mp4").first().download(output_path=temp_path, filename="temp_audio.mp4")
-
-        print(yt.title)
-
-        if yt.length < max_length:
-            result = {"Message": "Empty"}
+# Set up a background worker to transcribe videos
+def transcribe_worker():
+    while True:
+        # Pop job ID and video URL from Upstash QStash queue
+        job_info = r.brpop('qstash:transcription_jobs', timeout=0)
+        
+        if job_info is not None:
+            # Extract job ID and video URL from job info
+            job_id = json.loads(job_info[1])['job_id']
+            video_url = json.loads(job_info[1])['video_url']
+            api_key = json.loads(job_info[1])["api_key"]
             
+            # Update job status to 'in progress'
+            r.set('job:{}:status'.format(job_id), 'in progress')
+            
+            # Create temp path
+            temp_path = tempfile.gettempdir()
+
+            # Download the video from the URL
+            youtube = pytube.YouTube(video_url)
+            youtube.streams.filter(only_audio=True, file_extension="mp4").first().download(output_path=temp_path, filename="temp_audio.mp4")
+            
+            # Transcribe the video using OpenAI API
             with open(os.path.join(temp_path, "temp_audio.mp4"), "rb") as audio_file:
-                # Get the file size in bytes
-                file_size = os.fstat(audio_file.fileno()).st_size
-                print(f"The file size is {file_size} bytes.")
+                transcription = openai.Audio.transcribe("whisper-1", audio_file, api_key)
+            
+            # Store the transcription in the database
+            r.set('job:{}:transcription'.format(job_id), transcription["text"])
+            
+            # Update job status to 'completed'
+            r.set('job:{}:status'.format(job_id), 'completed')
+        
+        time.sleep(1)
 
-                # Transcribe the chunk using the OpenAI whisper API
-                print("Transcribing...")
-                transcription = openai.Audio.transcribe("whisper-1", audio_file)
-                print("Process finished")
+# Start the worker as a separate thread
+import threading
+worker_thread = threading.Thread(target=transcribe_worker)
+worker_thread.start()
 
-                result = {
-                    "script": transcription["text"],
-                    "length": yt.length,
-                    "title": yt.title,
-                    "views": yt.views,
-                    "description": yt.description
-                }
-
-            print(result)
-            return jsonify(result)
-        else:
-            print(f"Video is longer than {int(round(max_length/60, 0))} minutes")
-            return {"Too long": f"Video is longer than {int(round(max_length/60, 0))} minutes"}
-
-        # os.remove(os.path.join(temp_path, "temp_audio.mp4"))
-
-    except Exception as error:
-        print(error)
+if __name__ == '__main__':
+    app.run()
