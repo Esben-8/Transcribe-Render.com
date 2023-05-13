cc1899df5d34ad0da1446b97ea5ea19c4f8ce3aa
Esben <116391208+Esben-8@users.noreply.github.com>
2023-05-13 17:37:35 +0200

    PLEASE.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from pytube import YouTube
 import openai
 import tempfile
 import os
 import traceback
 import logging
-from pydub import AudioSegment
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -16,68 +15,56 @@ logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 @app.route("/")
 def hello():
     print("hej")
-    print("med")
-    return "Hello, !"
+    return "Hello"
 
 @app.route("/transcribe", methods=["GET"])
 def transcribe():
-#     try:
-    video_id = str(request.args.get("id"))
-    url = f"https://www.youtube.com/watch?v={video_id}"
-    # print(url)
-    max_length = int(request.args.get("max_length"))
-    apikey = str(request.args.get("api_key"))
-    print(apikey)
+    try:
+        video_id = str(request.args.get("id"))
+        url = f"https://www.youtube.com/watch?v={video_id}"
+        # print(url)
+        max_length = int(request.args.get("max_length"))
+        apikey = str(request.args.get("api_key"))
+        print(apikey)
 
-    openai.api_key = apikey
-    temp_path = tempfile.gettempdir()
-    yt = YouTube(url)
+        openai.api_key = apikey
+        temp_path = tempfile.gettempdir()
+        yt = YouTube(url)
 
-    print("Downloading")
-    yt.streams.filter(only_audio=True, file_extension="mp4").first().download(output_path=temp_path, filename="temp_audio.mp4")
+        print("Downloading...")
+        yt.streams.filter(only_audio=True, file_extension="mp4").first().download(output_path=temp_path, filename="temp_audio.mp4")
 
-    print(yt.title)
+        print(yt.title)
 
-    if yt.length < max_length:
-        result = {"Message": "Empty"}
-        
-        with open(os.path.join(temp_path, "temp_audio.mp4"), "rb") as audio_file:
-            # Get the file size in bytes
-            file_size = os.fstat(audio_file.fileno()).st_size
-            print(f"The file size is {file_size} bytes.")
+        if yt.length < max_length:
+            result = {"Message": "Empty"}
             
-            # Split the audio file into smaller chunks
-            audio = AudioSegment.from_file(audio_file, format="mp4")
-            chunk_size = 10 * 60 * 1000
-            chunks = audio[:chunk_size]
+            with open(os.path.join(temp_path, "temp_audio.mp4"), "rb") as audio_file:
+                # Get the file size in bytes
+                file_size = os.fstat(audio_file.fileno()).st_size
+                print(f"The file size is {file_size} bytes.")
 
-            transcriptions = []
-            for i, chunk in enumerate(chunks):
-                print(f"Transcribing chunk {i + 1}/{len(chunks)}")
-                with tempfile.NamedTemporaryFile(suffix=".mp4") as temp_chunk_file:
-                    chunk.export(temp_chunk_file.name, format="mp4")
-                    temp_chunk_file.seek(0)
+                # Transcribe the chunk using the OpenAI whisper API
+                print("Transcribing...")
+                transcription = openai.Audio.transcribe("whisper-1", audio_file)
+                print(transcription)
 
-                    # Transcribe the chunk using the OpenAI whisper API
-                    print("Transcribing")
-                    transcription = openai.Audio.transcribe("whisper-1", temp_chunk_file)
-                    transcriptions.append(transcription["text"])
+                result = {
+                    "script": transcription["text"],
+                    "length": yt.length,
+                    "title": yt.title,
+                    "views": yt.views,
+                    "description": yt.description
+                }
+            
+            print("Proccess finished")
+            return jsonify(result)
+        else:
+            print(f"Video is longer than {int(round(max_length/60, 0))} minutes")
+            return {"Too long": f"Video is longer than {int(round(max_length/60, 0))} minutes"}
 
-            result = {
-                "script": " ".join(transcriptions),
-                "length": yt.length,
-                "title": yt.title,
-                "views": yt.views,
-                "description": yt.description
-            }
+        # os.remove(os.path.join(temp_path, "temp_audio.mp4"))
 
-        return jsonify(result)
-    else:
-        print(f"Video is longer than {int(round(max_length/60, 0))} minutes")
-        return {"Too long": f"Video is longer than {int(round(max_length/60, 0))} minutes"}
-
-    os.remove(os.path.join(temp_path, "temp_audio.mp4"))
-
-#     except Exception as error:
-#         print(error)
-#         return jsonify({"Too long": str(error)})
+    except Exception as error:
+        print(error)
+        return make_response(jsonify(error), 500)
